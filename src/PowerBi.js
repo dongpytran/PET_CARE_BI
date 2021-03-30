7608713725f2d31c13c42265dc2784ad11f03a8e
unknown <dongpywebdev@gmail.com>
2021-03-30 14:04:59 +0700

    first commit

---
+++ src/PowerBi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./PowerBi.css"
+
+function PowerBi() {
+    return (
+        <div className="powerbi">
+            <iframe title="Petcare" src="https://app.powerbi.com/reportEmbed?reportId=b87804f9-d76c-4837-9ee2-0cbb26450a57&autoAuth=true&ctid=1daa5bb9-db94-40c3-b816-be04a9d39a90&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWQtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameBorder="0" allowFullScreen="true"></iframe>
+        </div>
+    )
+}
+
+export default PowerBi
