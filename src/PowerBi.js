9d27006137c4e7fccd604a98fed4980516a278ff
Dong Py Tran <67514451+dongpytran@users.noreply.github.com>
2021-03-30 22:45:58 +0700

    Update PowerBi.js

--- src/PowerBi.js
+++ src/PowerBi.js
@@ -4,7 +4,7 @@ import "./PowerBi.css"
 function PowerBi() {
     return (
         <div className="powerbi">
-            <iframe title="Petcare" src="https://app.powerbi.com/reportEmbed?reportId=b87804f9-d76c-4837-9ee2-0cbb26450a57&autoAuth=true&ctid=1daa5bb9-db94-40c3-b816-be04a9d39a90&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWQtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameBorder="0" allowFullScreen="true"></iframe>
+            <iframe title="Petcare" src="https://app.powerbi.com/reportEmbed?reportId=203faf4b-7135-40e4-aaca-5a705c3cbeb6&autoAuth=true&ctid=1daa5bb9-db94-40c3-b816-be04a9d39a90&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWQtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameBorder="0" allowFullScreen="true"></iframe>
         </div>
     )
 }
